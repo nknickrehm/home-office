32fde53115f70118c4620cd6094cbceaf8b34303
Nikolas Knickrehm <nikolas.knickrehm@isp.uni-luebeck.de>
2020-03-22 13:11:26 +0100

    Added user interface for creating courses and viewing course details

--- routes/plattform.js
+++ routes/plattform.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const Course = require('../models/course').Course;
+const User = require('../models/user').User;
 
 var router = express.Router();
 
@@ -14,12 +15,44 @@ router.get('/alle-kurse', function(req, res, next) {
     if (err) return next(err);
     res.render('all-courses', { loggedIn: req.isAuthenticated(), courses });
   });
+});
 
+/* GET users listing. */
+router.get('/alle-kurse/:courseId/', function(req, res, next) {
+  Course.findOne({ _id: req.params.courseId }, (err, course) => {
+    if (err) return next(err);
+    res.render('course', { loggedIn: req.isAuthenticated(), course });
+  });
 });
 
 /* GET users listing. */
 router.get('/mein-profil', function(req, res, next) {
   res.render('my-profile', { loggedIn: req.isAuthenticated() });
+});
+
+
+/* GET users listing. */
+router.get('/kurs-erstellen', function(req, res, next) {
+  res.render('create-course', { loggedIn: req.isAuthenticated() });
+});
+
+router.post('/kurs-erstellen', function(req, res, next) {
+  const { body } = req;
+  const {
+    title,
+    description } = body;
+
+  if (!title || !description)
+    return res.redirect('/kurs-erstellen');
+
+  const newCourse = new Course({ title, description });
+  newCourse.save((err, course) => {
+    if (err) return next(err);
+    User.findOneAndUpdate({ _id: req.user._id }, { $push: { courses: course } }, (err) => {
+      if (err) return next(err);
+      return res.redirect(`/plattform/alle-kurse/${ course._id }`);
+    });
+  });
 });
 
 module.exports = router;
